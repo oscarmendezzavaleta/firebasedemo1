aa16ca2ecb859deeb6399469d15cf8467d7167fb
USERSAD\omendezz <oscar.mendez.zavaleta@gmail.com>
2018-06-25 15:31:14 -0500

    agregar toas

--- app/src/main/java/com/example/pcoscar/firebasedemo1/login.java
+++ app/src/main/java/com/example/pcoscar/firebasedemo1/login.java
@@ -1,5 +1,6 @@
 package com.example.pcoscar.firebasedemo1;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -61,6 +63,7 @@ public class login extends AppCompatActivity implements View.OnClickListener {
                 if(task.isSuccessful()) {
                     Log.i("SESION", "usuario creado correctamente");
                 }else{
+                    Toast.makeText(login.this      ,task.getException().getMessage().toString(), Toast.LENGTH_LONG).show();
                       Log.e("SESION",task.getException().getMessage()+"");
                     }
 
